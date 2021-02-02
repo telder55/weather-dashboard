d0012d26db418434d85a7d0891f9d0056add71de
TJ Elder <telder55@gmail.com>
2021-02-02 16:28:26 -0700

    Added basic html and css

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={3a551c8fc73e4638743bd3a7d220e283}";
+console.log(apiURL);
+
+
+// Call API by City Name
+var denverWeather = 'api.openweathermap.org/data/2.5/weather?q={Denver}&appid={3a551c8fc73e4638743bd3a7d220e283}';
+
+console.log(denverWeather);
