cb51afbaf91bdec4f49c4a0be6aa8a7c65d82c49
TJ Elder <telder55@gmail.com>
2021-02-08 10:14:01 -0700

    Added Icon to Current Weather Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// var city = "denver";
-// var currentCity = "Chicago";
 var currentCardParent = document.getElementById("current-area");
 var currentCard = document.createElement("div");
 
@@ -27,10 +25,15 @@ function getWeather(city) {
                 console.log("Current Humidity: ", data.main.humidity);
                 //logs windspeed in MPH
                 console.log("Current Wind Speed: ", data.wind.speed);
+                //Logs Icon
+                console.log("Icon: ", data.weather[0].icon);
                 // Logs and Longitude
                 console.log("Longitude:", data.coord.lon);
                 // Logs and Longitude
                 console.log("Latitude:", data.coord.lat);
+                
+                // Creating URL for icon image
+                var iconURL = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 
                 // Adding to Screen
                 currentCard.innerHTML = "";
@@ -38,24 +41,31 @@ function getWeather(city) {
                 var cityTemp = document.createElement("p");
                 var cityHumidity = document.createElement("p");
                 var cityWind = document.createElement("p");
-                
-            
+               
+
+                //Filling in Elements
                 cityDate.textContent = city + " " + dt.toLocaleString();
                 cityTemp.textContent = "Temperature: " + data.main.temp + " F";
                 cityHumidity.textContent = "Humidity: " + data.main.humidity + "%";
                 cityWind.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+                
+                // Creating IMG Tag with ID #current-icon
+                $('#current-area').html($('<img>', {id: 'current-icon'}, {src: 'iconURL'}));
+                // Adding src URL to IMG Tag 
+                $('#current-icon').attr('src', iconURL);
+
+                //Appending Elements
                 currentCard.appendChild(cityDate);
                 currentCard.appendChild(cityTemp);
                 currentCard.appendChild(cityHumidity);
                 currentCard.appendChild(cityWind);
                 currentCardParent.appendChild(currentCard);
 
+                // Adding URL to DOM
+                $('#weather-icon').attr('src', iconURL);
+
+                // Calling UV Index Function
                 getUVI(data.coord.lon, data.coord.lat);
-
-                // Change this function to make display!
-                //   displayRepos(data, user);
-
-                //pass coordinates to new function and then define the cityWeatherOneCall variable within that function and do a fetch there
 
             });
 
@@ -69,11 +79,13 @@ function getWeather(city) {
 
     // Getting UV Index
     function getUVI(lon, lat) {
-        var cityWeatherOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=3a551c8fc73e4638743bd3a7d220e283`;
+        var cityWeatherOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=3a551c8fc73e4638743bd3a7d220e283`;
         fetch(cityWeatherOneCall).then(function (response) {
             if (response.ok) {
                 console.log(response);
                 response.json().then(function (data) {
+                    //logs all current weather data in object
+                    console.log(data);
                     //Logs Current UVI
                     console.log("Current UV Index: ", data.current.uvi);
 
@@ -87,9 +99,22 @@ function getWeather(city) {
                     } else if (data.current.uvi >= 8 && data.current.uvi <= 10) {
                         cityUV.classList.add("red");
                     } else cityUV.classList.add("green")
-                    
+
                     currentCard.appendChild(cityUV);
-                    
+
+                    // 5-Day Forecast Section
+                    // Day 1 Date 
+                    console.log();
+                    // Day 1 icon
+                    console.log(data.daily[1].weather[0].icon);
+                    // Day 1 Temp
+                    console.log(data.daily[1].temp.day);
+                    // Day 1 Humidity
+                    console.log(data.daily[1].humidity);
+
+
+                    // For Loop to move through array and grab 5-day forecast data. 
+
 
 
                 });
